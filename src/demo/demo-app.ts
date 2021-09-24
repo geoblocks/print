df839bc2e31a3a4aae4b1971540f7f4ede47aada
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2021-09-24 17:48:06 +0200

    Show both demos

--- src/demo/demo-app.ts
+++ src/demo/demo-app.ts
@@ -26,16 +26,12 @@ import Text from 'ol/style/Text';
 import stylefunction from "ol-mapbox-style/dist/stylefunction";
 import TileGrid from 'ol/tilegrid/TileGrid';
 import {extentFromProjection} from "ol/tilegrid";
+import {olCss} from './css';
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
+
 @customElement('demo-app')
 export class DemoApp extends LitElement {
-  static styles = css`
+  static styles = [css`
     :host {
       display: block;
       border: solid 1px gray;
@@ -56,92 +52,9 @@ export class DemoApp extends LitElement {
       display: inline-block;
       width: 45%;
       height: calc(100vh - 32px - 20px - 40px);
-    }
-    .ol-control {
-      position: absolute;
-      background-color: rgba(255,255,255,0.4);
-      border-radius: 4px;
-      padding: 2px;
-    }
-    .ol-control:hover {
-      background-color: rgba(255,255,255,0.6);
-    }
-    .ol-zoom {
-      top: .5em;
-      left: .5em;
-    }
-    .ol-control button {
-      display: block;
-      margin: 1px;
-      padding: 0;
-      color: white;
-      font-size: 1.14em;
-      font-weight: bold;
-      text-decoration: none;
-      text-align: center;
-      height: 1.375em;
-      width: 1.375em;
-      line-height: .4em;
-      background-color: rgba(0,60,136,0.5);
-      border: none;
-      border-radius: 2px;
-    }
-    .ol-control button::-moz-focus-inner {
-      border: none;
-      padding: 0;
-    }
-    .ol-rotate {
-      top: .5em;
-      right: .5em;
-      transition: opacity .25s linear, visibility 0s linear;
-    }
-    .ol-rotate.ol-hidden {
-      opacity: 0;
-      visibility: hidden;
-      transition: opacity .25s linear, visibility 0s linear .25s;
-    }
-    .ol-attribution li {
-      display: inline;
-      list-style: none;
-    }
-    .ol-attribution li:not(:last-child):after {
-      content: " ";
-    }
-    .ol-attribution img {
-      max-height: 2em;
-      max-width: inherit;
-      vertical-align: middle;
-    }
-    .ol-attribution button {
-      flex-shrink: 0;
-    }
-    .ol-attribution.ol-collapsed ul {
-      display: none;
-    }
-    .ol-attribution:not(.ol-collapsed) {
-      background: rgba(255,255,255,0.8);
-    }
-    .ol-attribution.ol-uncollapsible {
-      bottom: 0;
-      right: 0;
-      border-radius: 4px 0 0;
-    }
-    .ol-attribution.ol-uncollapsible img {
-      margin-top: -.2em;
-      max-height: 1.6em;
-    }
-    .ol-attribution.ol-uncollapsible button {
-      display: none;
-    }
-    .ol-viewport, .ol-unselectable {
-      user-select: none;
-    }
-    .ol-selectable {
-      user-select: text;
-    }
-
-
-  `
+    }`,
+    olCss
+  ];
 
   @query('#map')
   private mapEl?: HTMLElement;
@@ -162,81 +75,33 @@ export class DemoApp extends LitElement {
     fetch(url)
     .then(r => r.json())
     .then(style => {
-        let spriteUrl, spriteDataUrl, spriteImageUrl, addMpFonts;
+      let spriteUrl, spriteDataUrl, spriteImageUrl, addMpFonts;
 
-        if (style.sprite) {
-            spriteUrl = style.sprite;
+      if (style.sprite) {
+        spriteUrl = style.sprite;
 
-            // support relative spriteUrls
-            if (spriteUrl.includes("./")) {
-                spriteUrl = new URL(spriteUrl, url);
-            }
-
-            spriteDataUrl = spriteUrl.toString().concat(".json");
-            spriteImageUrl = spriteUrl.toString().concat(".png");
-
-            fetch(spriteDataUrl)
-                .then(r => r.json())
-                .then(r => r.data)
-                .then(spriteData => {
-                    stylefunction(layer, style, Object.keys(style.sources)[0], undefined, spriteData, spriteImageUrl, addMpFonts);
-                }
-                );
+        // support relative spriteUrls
+        if (spriteUrl.includes("./")) {
+            spriteUrl = new URL(spriteUrl, url);
         }
-        else {
-            stylefunction(layer, style, Object.keys(style.sources)[0], undefined, undefined, undefined, addMpFonts);
-        }
+
+        spriteDataUrl = spriteUrl.toString().concat(".json");
+        spriteImageUrl = spriteUrl.toString().concat(".png");
+
+        fetch(spriteDataUrl)
+          .then(r => r.json())
+          .then(r => r.data)
+          .then(spriteData => {
+            stylefunction(layer, style, Object.keys(style.sources)[0], undefined, spriteData, spriteImageUrl, addMpFonts);
+          });
+      }
+      else {
+        stylefunction(layer, style, Object.keys(style.sources)[0], undefined, undefined, undefined, addMpFonts);
+      }
     });
-
   }
 
   createMap() {
-    const extent = extentFromProjection('EPSG:3857');
-    const origin = [extent[0], extent[3]];
-    this.mvtLayer = new VectorTileLayer({
-      // style(feature) {
-      //   if (feature.getGeometry()?.getType() === 'Point') {
-      //     return new Style({
-      //       text: new Text({
-      //         font: 'bold 28px beach',
-      //         text: 'beach',
-      //         offsetY: 40,
-      //       }),
-      //       image: new Icon({
-      //         src: `/beach.svg`,
-      //         opacity: 0.5,
-      //         scale: 0.05,
-      //       })
-      //     })
-      //   }
-      //   return new Style({
-      //     stroke: new Stroke({
-      //       color: 'red'
-      //     }),
-      //   })
-      // },
-      source: new VectorTileSource({
-        format: new MVT(),
-        tileGrid: new TileGrid({
-          tileSize: 512,
-          resolutions: [78271.5169640117238, 39135.7584820058619,
-            19567.8792410029309, 9783.93962050146547, 4891.96981025073273,
-            2445.98490512536637, 1222.99245256268318, 611.496226281341592,
-            305.7481131406708, 152.8740565703354, 76.437028285167699,
-            38.2185141425838495, 19.1092570712919247, 9.55462853564596237,
-            4.77731426782298119, 2.38865713391149059, 1],
-            extent: extent,
-            origin: origin,
-        }),
-        url: 'https://adv-smart.de/tiles/smarttiles_de_public/{z}/{x}/{y}.pbf',
-        maxZoom: 14,
-        // extent: trackExtent,
-      })
-    });
-    this.configureVTStyle(this.mvtLayer,
-      //'https://dev.adv-smart.de/styles/public/v0/de_style_grey.json'
-      'https://adv-smart.de/styles/public/de_style_hillshade.json'
-    );
     this.printExtentLayer = new VectorLayer({
       // @ts-ignore we add custom property, which is fine with OL
       'name': 'printExtent',
@@ -247,23 +112,13 @@ export class DemoApp extends LitElement {
     this.map = new OLMap({
       target: this.mapEl,
       layers: [
-        // new TileLayer({
-        //   source: new OSM(),
-        // }),
-        this.printExtentLayer,
-        this.mvtLayer,
-        new TileLayer({
-          //zIndex: 10000,
-          source: new TileDebug({
-            tileGrid: this.mvtLayer.getSource().getTileGrid()
-          })
-        }),
       ],
       view: new View({
-        center: fromLonLat([8.355,47.576]),
         zoom: 14
       })
-    })
+    });
+    this.configureSimpleDemo();
+
     this.map.on('postcompose', evt => {
       const res = evt.frameState.viewState.resolution;
       //const printResolution = 1 / PIXELS_PER_METER / this.printScale;
@@ -304,6 +159,109 @@ export class DemoApp extends LitElement {
     this.result0 = result[0];
   }
 
+  configureSimpleDemo() {
+    this.mvtLayer = new VectorTileLayer({
+      style(feature) {
+        if (feature.getGeometry()?.getType() === 'Point') {
+          return new Style({
+            text: new Text({
+              font: 'bold 28px beach',
+              text: 'beach',
+              offsetY: 40,
+            }),
+            image: new Icon({
+              src: `/beach.svg`,
+              opacity: 0.5,
+              scale: 0.05,
+            })
+          })
+        }
+        return new Style({
+          stroke: new Stroke({
+            color: 'red'
+          }),
+        })
+      },
+      source: new VectorTileSource({
+        format: new MVT(),
+        url: '/tiles/{z}/{x}/{y}.pbf',
+        maxZoom: 14,
+        // extent: trackExtent,
+      })
+    });
+
+    const layers = this.map?.getLayers();
+    layers!.clear();
+    const newLayers = [
+      new TileLayer({
+        source: new OSM(),
+      }),
+      this.mvtLayer!,
+      this.printExtentLayer!,
+      new TileLayer({
+        zIndex: 10000,
+        source: new TileDebug({
+          tileGrid: this.mvtLayer.getSource().getTileGrid()
+        })
+      }),
+    ];
+    layers!.extend(newLayers);
+    this.map?.getView().setCenter(fromLonLat([6.57253, 46.51336]));
+  }
+
+  configureMapboxDemo() {
+    const extent = extentFromProjection('EPSG:3857');
+    const origin = [extent[0], extent[3]];
+    this.mvtLayer = new VectorTileLayer({
+      source: new VectorTileSource({
+        format: new MVT(),
+        tileGrid: new TileGrid({
+          tileSize: 512,
+          resolutions: [78271.5169640117238, 39135.7584820058619,
+            19567.8792410029309, 9783.93962050146547, 4891.96981025073273,
+            2445.98490512536637, 1222.99245256268318, 611.496226281341592,
+            305.7481131406708, 152.8740565703354, 76.437028285167699,
+            38.2185141425838495, 19.1092570712919247, 9.55462853564596237,
+            4.77731426782298119, 2.38865713391149059, 1],
+            extent: extent,
+            origin: origin,
+        }),
+        url: 'https://adv-smart.de/tiles/smarttiles_de_public/{z}/{x}/{y}.pbf',
+        maxZoom: 14,
+      })
+    });
+    this.configureVTStyle(this.mvtLayer,
+      //'https://dev.adv-smart.de/styles/public/v0/de_style_grey.json'
+      'https://adv-smart.de/styles/public/de_style_hillshade.json'
+    );
+
+    const layers = this.map?.getLayers();
+    layers!.clear();
+    const newLayers = [
+      this.mvtLayer!,
+      this.printExtentLayer!,
+      new TileLayer({
+        zIndex: 10000,
+        source: new TileDebug({
+          tileGrid: this.mvtLayer.getSource().getTileGrid()
+        })
+      }),
+    ];
+    layers!.extend(newLayers);
+    this.map?.getView().setCenter(fromLonLat([8.355,47.576]));
+  }
+
+  updateDemo(demo: string) {
+    switch (demo) {
+      case 'simple':
+        this.configureSimpleDemo();
+        break;
+      case 'mapbox':
+        this.configureMapboxDemo();
+        break;
+    }
+  }
+
   render() {
     let img: TemplateResult|'' = '';
     let extent: TemplateResult|'' = '';
@@ -315,6 +273,11 @@ export class DemoApp extends LitElement {
       extent = html`<div>${JSON.stringify(toLonLat(c0, 'EPSG:3857'))} ${JSON.stringify(toLonLat(c1, 'EPSG:3857'))}</div>`;
     }
     return html`
+    <label for="demo-select">Choose a demo:</label>
+      <select name="demos" id="demo-select" value="mapbox" @change=${(evt) => this.updateDemo(evt.target.value)}>
+        <option value="simple">Basic style function</option>
+        <option value="mapbox">OL-Mapbox-style</option>
+      </select>
       <div>${extent || 'Move around and click the print button...'}</div>
       <div>zoom: ${this.zoom.toFixed(1)}</div>
       <div>
