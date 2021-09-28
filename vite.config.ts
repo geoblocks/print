172cbe3a945e6cd8521b1411c57eba3c3ecf8d0a
Guillaume Beraudo <guillaume.beraudo@camptocamp.com>
2021-09-28 16:57:04 +0200

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  target: "ES2020",
   build: {
     lib: {
       entry: 'src/main.ts',
       formats: ['es']
     },
     rollupOptions: {
-      external: /^lit/,
+      external: /^lit|ol/,
       input: {
         demo: 'src/main.ts'
       }
